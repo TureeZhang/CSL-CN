a2b4ae92e7e8456ea3bd1de9a580da6f972cef6c
TureeZhang <aa940724@outlook.com>
2020-03-19 23:55:08 +0800

    feat:LogService

---
+++ HanJie.CSLCN.Models/Dtos/LogDto.cs
@@ -0,0 +1,15 @@
+﻿using HanJie.CSLCN.Models.DataModels;
+using HanJie.CSLCN.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HanJie.CSLCN.Models.Dtos
+{
+    public class LogDto : BaseDtoModel<LogDto, Log>
+    {
+        public LogLevelEnum LogLevel { get; set; }
+        public string Message { get; set; }
+        public string ParametersJson { get; set; }
+    }
+}
---
+++ HanJie.CSLCN.Models/Enums/LogLevelEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HanJie.CSLCN.Models.Enums
+{
+    public enum LogLevelEnum
+    {
+        Info,
+        Warning,
+        Error
+    }
+}
