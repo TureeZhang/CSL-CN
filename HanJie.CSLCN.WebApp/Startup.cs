ac104f5b7ff27c4fc269b3b49b36b8187be02746
TureeZhang <aa940724@outlook.com>
2020-11-17 02:01:16 +0800

    fix: register filter

--- HanJie.CSLCN.WebApp/Startup.cs
+++ HanJie.CSLCN.WebApp/Startup.cs
@@ -37,7 +37,10 @@ namespace HanJie.CSLCN.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new RequestLogFilter());
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the Angular files will be served from this directory
             //services.AddSpaStaticFiles(configuration =>
